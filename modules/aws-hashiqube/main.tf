6e75e98a292989cb13466bb1529cdaaf23e1f30f
Riaan Nolan <riaan.nolan@gmail.com>
2023-11-05 09:38:40 +1000

    switch to ubuntu focal -> jammy and cgroups v1 -> v2

--- modules/aws-hashiqube/main.tf
+++ modules/aws-hashiqube/main.tf
@@ -42,7 +42,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
   filter {
     name   = "virtualization-type"
